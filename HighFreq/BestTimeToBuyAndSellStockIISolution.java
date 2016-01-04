96a796b565d92ab93ede602c9db469fb61af0584
Hongying <tsingpg@hotmail.com>
2016-01-03 22:55:25 -0500

    fix small typos

--- HighFreq/BestTimeToBuyAndSellStockIISolution.java
+++ HighFreq/BestTimeToBuyAndSellStockIISolution.java
@@ -1,33 +1,14 @@
-public class  BestTimeToBuyAndSellStockIISolution {
-    /* //version 1
+public class BestTimeToBuyAndSellStockSolutionIISolution {
+    public int maxProfit(int[] prices) {
         if(prices == null || prices.length == 0){
             return 0;
         }
-        int high = prices[0];
-        int low =prices[0];
-        int profit = 0;
+        int result = 0;
         for(int i = 1; i < prices.length; i++){
-            if(prices[i] < low){
-                low = prices[i];
-                high = low;
-            }else if( prices[i] > high){
-                high = prices[i];
-                if(high - low > profit){
-                    profit = high - low;
-                }
+            if(prices[i] >= prices[i-1]){
+                result += prices[i] - prices[i-1];
             }
         }
-        return profit;
-        */
-        //version 2
-        if(prices == null || prices.length < 2){
-            return 0;
-        }
-        int max = Integer.MIN_VALUE; //profit
-        int min = Integer.MAX_VALUE;
-        for(int i : prices){
-            min = Math.min(min, i);
-            max = Math.max(max, i - min);
-        }
-        return max;
+        return result;
+    }
 }
---
+++ HighFreq/BestTimeToBuyAndSellStockSolution.java
@@ -0,0 +1,33 @@
+public class  BestTimeToBuyAndSellStockSolution {
+    /* //version 1
+        if(prices == null || prices.length == 0){
+            return 0;
+        }
+        int high = prices[0];
+        int low =prices[0];
+        int profit = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] < low){
+                low = prices[i];
+                high = low;
+            }else if( prices[i] > high){
+                high = prices[i];
+                if(high - low > profit){
+                    profit = high - low;
+                }
+            }
+        }
+        return profit;
+        */
+        //version 2
+        if(prices == null || prices.length < 2){
+            return 0;
+        }
+        int max = Integer.MIN_VALUE; //profit
+        int min = Integer.MAX_VALUE;
+        for(int i : prices){
+            min = Math.min(min, i);
+            max = Math.max(max, i - min);
+        }
+        return max;
+}
