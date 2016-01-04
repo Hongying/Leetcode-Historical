f99d473f4f8679ed7cd33a6ca580cb269bf18a53
Hongying <tsingpg@hotmail.com>
2016-01-03 22:31:33 -0500

    Best Time to Buy and Sell Stock update

--- HighFreq/BestTimeToBuyAndSellStockIISolution.java
+++ HighFreq/BestTimeToBuyAndSellStockIISolution.java
@@ -1,14 +1,33 @@
 public class  BestTimeToBuyAndSellStockIISolution {
-    public int maxProfit(int[] prices) {
+    /* //version 1
         if(prices == null || prices.length == 0){
             return 0;
         }
-        int result = 0;
+        int high = prices[0];
+        int low =prices[0];
+        int profit = 0;
         for(int i = 1; i < prices.length; i++){
-            if(prices[i] >= prices[i-1]){
-                result += prices[i] - prices[i-1];
+            if(prices[i] < low){
+                low = prices[i];
+                high = low;
+            }else if( prices[i] > high){
+                high = prices[i];
+                if(high - low > profit){
+                    profit = high - low;
+                }
             }
         }
-        return result;
-    }
+        return profit;
+        */
+        //version 2
+        if(prices == null || prices.length < 2){
+            return 0;
+        }
+        int max = Integer.MIN_VALUE; //profit
+        int min = Integer.MAX_VALUE;
+        for(int i : prices){
+            min = Math.min(min, i);
+            max = Math.max(max, i - min);
+        }
+        return max;
 }
