7388d06b806c05cc001996dc4e6d71c3244f2e38
Hongying <tsingpg@hotmail.com>
2016-02-13 17:06:23 -0500

    Search a 2D Matrix II

---
+++ BinarySearch/SearchA2DMatrixIISolution.java
@@ -0,0 +1,19 @@
+public class SearchA2DMatrixIISolution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0){
+            return false;
+        }
+        int i = matrix.length - 1;
+        int j = 0;
+        while(i >= 0 && j < matrix[0].length){
+            if(matrix[i][j] == target){
+                return true;
+            }else if(matrix[i][j] < target){
+                j++;
+            }else{
+                i--;
+            }
+        }
+        return false;
+    }
+}
