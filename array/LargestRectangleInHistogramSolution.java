31b118f50f2aca4a8d987536d5a6143aad8d30ea
Hongying <tsingpg@hotmail.com>
2016-02-21 16:48:53 -0500

    Largest Rectangle in Histogram

---
+++ array/LargestRectangleInHistogramSolution.java
@@ -0,0 +1,40 @@
+public class LargestRectangleInHistogramSolution {
+    public int largestRectangleArea(int[] heights) {
+        if(heights == null || heights.length == 0){
+            return 0;
+        }
+        //version 1 brute force O(n^2), fix a index and expand to both sides to find the edge of the rectangle
+        /*
+        int area = 0;
+        for(int i = 0; i < heights.length; i++){
+            int left = i;
+            while(left >= 0 && heights[left] >= heights[i]){
+                left--;
+            }
+            left++;
+            int right = i;
+            while(right < heights.length && heights[right] >= heights[i]){
+                right++;
+            }
+            right--;
+            area = Math.max(area, heights[i] * (right - left + 1));
+        }
+        return area;
+        */
+        int area = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(-1);
+        for (int i = 0; i < heights.length; i++) {
+            while(stack.peek() != -1 && heights[stack.peek()] >= heights[i]){
+                int top = stack.pop();
+                area = Math.max(area, heights[top] * (i - 1 - (stack.peek() + 1) + 1));
+            }
+            stack.push(i);
+        }
+        while(stack.size() > 1){
+            int x = stack.pop();
+            area = Math.max(area, (heights.length - 1 - (stack.peek() + 1) + 1) * heights[x]);
+        }
+        return area;
+    }
+}
